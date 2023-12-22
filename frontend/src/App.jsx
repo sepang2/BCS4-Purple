30b6e2be788c13dccb9aeb8e5324a57742e6cd0c
Sepang2 <sepang296@gmail.com>
2023-12-22 18:02:39 +0900

    231222

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Mint from "./pages/mint";
+import Coin from "./pages/coin";
+import Detail from "./pages/detail";
+
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mint" element={<Mint />} />
+        <Route path="/coin" element={<Coin />} />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <div className="bg-red-100">
+      <Header />
+      Home
+    </div>
+  );
+};
+
+export default Home;
